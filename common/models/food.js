45ce74a1878083c97426d9dc167ed9208c8cb962
marcos8896 <elyomarcos@gmail.com>
2018-06-26 14:07:37 -0500

    Fixed #1 - Replicated food model functionality.

---
+++ common/models/food.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(Food) {
+
+  Food.validatesLengthOf('description', {max: 80, message: {max: 'La descripción no debe sobrepasar los 80 caracteres.'}});
+
+};
