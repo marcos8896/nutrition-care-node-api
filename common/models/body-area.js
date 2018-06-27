4cbfb6186c5e6849305a4fe91ae1e0ad1faddffc
marcos8896 <elyomarcos@gmail.com>
2018-06-26 21:50:32 -0500

    #Fixed #7 - Added Body Area model to the project.

---
+++ common/models/body-area.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(Bodyarea) {
+
+  Bodyarea.validatesLengthOf('description', {max: 80, message: {max: 'La descripción no debe sobrepasar los 80 caracteres.'}});
+
+};
