8adef752098ac4cf9aaa084c7168d73e7a8eef4d
marcos8896 <elyomarcos@gmail.com>
2018-06-28 22:09:55 -0500

    #10 - Change the node modules definition to a class definition for the customer.service.js file.

--- common/models/customer.js
+++ common/models/customer.js
@@ -9,12 +9,7 @@ module.exports = Customer => {
   //Before Remote Hooks --->
   Customer.beforeRemote('create', customerService.beforeRemoteCreate);
 
-  /**
-   * @author Marcos Barrera del Río <elyomarcos@gmail.com>
-   * @description: 'type' property will be added to the response that will be 
-   * returned in the login request. With 'type' property, the client
-   * application will know which type of user is loggin in
-   */
+  //After Remote Hooks --->
   Customer.afterRemote('login', customerService.afterRemoteLogin);
 
 };
--- common/services/customer.service.js
+++ common/services/customer.service.js
@@ -1,36 +1,42 @@
-'use strict';
 
-const  uuid = require('uuid/v4');
-const { waterfall } = require('async');
 const app = require('../../server/server');
 const CONSTANTS_ROLES = require('../../shared/constants-roles');
 
-/**
- * @author Marcos Barrera del Río <elyomarcos@gmail.com>
- * @description:
- * 'type' property will be added to the body request to ensure
- * that every new customer user is regitered as REGULAR customer
- * user.
- */
-function beforeRemoteCreate(ctx, unused, next) {
-  ctx.req.body.type = CONSTANTS_ROLES.CUSTOMER.REGULAR;
-  next();
+const uuid = require('uuid/v4');
+const { waterfall } = require('async');
+
+class CustomerService {
+
+  /**
+   * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+   * @description: 'type' property will be added to the body request to ensure
+   * that every new customer user is regitered as REGULAR customer
+   * user.
+   */
+  static beforeRemoteCreate(ctx, unused, next) {
+    ctx.req.body.type = CONSTANTS_ROLES.CUSTOMER.REGULAR;
+    next();
+  }
+
+  /**
+   * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+   * @description: 'type' property will be added to the response that will be 
+   * returned in the login request. With 'type' property, the client
+   * application will know which type of user is loggin in
+   */
+  static afterRemoteLogin(ctx, response, next) {
+
+    const RoleMapping = app.models.RoleMapping;
+
+    app.models.Customer.findById(response.userId)
+      .then( user => {
+        response.type = user.type;
+        next();
+      })
+      .catch( err => next(err));
+    
+  }
+
 }
 
-function afterRemoteLogin(ctx, response, next) {
-
-  const RoleMapping = app.models.RoleMapping;
-
-  app.models.Customer.findById(response.userId)
-    .then( user => {
-      response.type = user.type;
-      next();
-    })
-    .catch( err => next(err));
-  
-}
-
-module.exports = {
-  beforeRemoteCreate,
-  afterRemoteLogin
-}
+module.exports = CustomerService;
