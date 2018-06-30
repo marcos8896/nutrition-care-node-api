a498d40b886b38edab616040f6332a1809bb4303
marcos8896 <elyomarcos@gmail.com>
2018-06-29 21:17:51 -0500

    #5 - Now every time a new property is added to a Loopback model, by running the 'prepare-seeds.js' script, this new property will be added to the Seed model automatically.

--- dev/seeds/prepare-seeds.js
+++ dev/seeds/prepare-seeds.js
@@ -1,12 +1,31 @@
 const asyncSeries = require('async').series;
 const fileExists = require('file-exists');
 const mkdirp = require('mkdirp');
-const seedsDirectory = '/dev/seeds/seedModels/'
+const readfiles = require('node-readfiles');
+const seedsDirectory = '/dev/seeds/seedModels/';
+
 let arrayModels = [];
 
 
+
+
+asyncSeries([
+  cb => getModelsContentFromJSONs(cb),
+  cb => prepareSeedsModels(cb),
+  cb => checkIfDirectoryExists(cb),
+  cb => checkJSONSeedsAvailability(cb),
+  cb => addNewPropertiesFromJSONModelsToSeedModels(cb),
+  cb => writeRemainingJSONFiles(cb),
+], (err, results) => {
+  if(err) console.log('Error on asyncSeries - prepare-seeds.js: ', err);
+  else console.log("\nTodo bien, men.");
+  
+});
+
+
+
+
 function getModelsContentFromJSONs(cb) {
-  const readfiles = require('node-readfiles');
 
   readfiles('./common/models/', { filter: '*.json' }, (err, filename, contents) => {
     if (err) throw err;
@@ -19,12 +38,14 @@ function getModelsContentFromJSONs(cb) {
 
     arrayModels.push(json);
   })
-    .then(files => cb(null))
-    .catch(err => {
-      console.log('Error reading files:', err.message);
-      cb(err);
-    });
+  .then(files => cb(null))
+  .catch(err => {
+    console.log('Error reading files:', err.message);
+    cb(err);
+  });
+
 }
+
 
 function prepareSeedsModels(cb) {
   arrayModels.forEach(model =>
@@ -34,6 +55,15 @@ function prepareSeedsModels(cb) {
   );
   cb(null);
 }
+
+
+function checkIfDirectoryExists( cb ) {
+  mkdirp(`.${seedsDirectory}`, (err) => {
+    if (err) cb( err );
+    cb( null );
+  });
+}
+
 
 function checkJSONSeedsAvailability(cb) {
 
@@ -47,10 +77,63 @@ function checkJSONSeedsAvailability(cb) {
     )
   })
 
-
-
   Promise.all(promises).then(() => cb(null));
 }
+
+function addNewPropertiesFromJSONModelsToSeedModels( cb ) {
+
+  readfiles(`.${seedsDirectory}`, { filter: '*.json' }, (err, filename, jsonString) => {
+
+    //Parsed to an object the previous JSON seed file.
+    const seedObject = JSON.parse(jsonString);
+
+    //Get current object seed from array to check if they have the same properties.
+    const objectFromArrayModelsIndex = arrayModels.findIndex( model => {
+      return seedObject.name === model.name
+    });
+
+    const objectFromArrayModels = arrayModels[objectFromArrayModelsIndex];
+    
+    //Get every property of the previous seed object on a new array.
+    const currentSeedProperties = seedObject.properties_seeds.map( prop => {
+      return Object.keys(prop)[0];
+    });
+
+    //Get every property of the current seed object on a new array.
+    const currentModelProperties = objectFromArrayModels.properties_seeds.map( prop => {
+      return Object.keys(prop)[0];
+    });
+
+    //Get an array of all the properties that have to be added to the seed model.
+    const newPropertiesToWrite = currentModelProperties.filter(val => {
+      return !currentSeedProperties.includes(val);
+    });
+
+
+    if(newPropertiesToWrite.length > 0) {
+      
+      //Get current model from main array.
+      let current = arrayModels[objectFromArrayModelsIndex];
+
+      //Create an object to be added to the curretn properties_seeds on the seed model.
+      let newPropertiesObject = newPropertiesToWrite.map( prop => { 
+        return {[prop] : ""} 
+      });
+      
+      //Added all new properties to the current JSON model that will be re-write.
+      current.properties_seeds = [ ...newPropertiesObject, ...seedObject.properties_seeds ];
+      current.fileExists = false;
+    }
+
+  })
+  .then(files => cb(null))
+  .catch(err => {
+    console.log('Error reading files:', err.message);
+    cb(err);
+  });
+
+}
+
 
 function writeRemainingJSONFiles(cb) {
   let each = require('async').each;
@@ -77,22 +160,3 @@ function writeRemainingJSONFiles(cb) {
 
   })
 }
-
-function checkIfDirectoryExists( cb ) {
-  mkdirp(`.${seedsDirectory}`, (err) => {
-    if (err) cb( err );
-    cb( null );
-  });
-}
-
-asyncSeries([
-  cb => getModelsContentFromJSONs(cb),
-  cb => prepareSeedsModels(cb),
-  cb => checkIfDirectoryExists(cb),
-  cb => checkJSONSeedsAvailability(cb),
-  cb => writeRemainingJSONFiles(cb)
-], (err, results) => {
-  if(err) console.log('Error on asyncSeries - prepare-seeds.js: ', err);
-  else console.log("\nTodo bien, men.");
-  
-});
