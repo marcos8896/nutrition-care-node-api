454ed8f7adc2c3b0d943b94a3764c012720a428f
marcos8896 <elyomarcos@gmail.com>
2018-07-03 22:13:38 -0500

    #5 - Added utils/shared.js file to modularize in a better way the execute-seeds script.

--- dev/seeds/execute-seeds.js
+++ dev/seeds/execute-seeds.js
@@ -11,14 +11,19 @@
  */
 
 const series = require('async').series;
-const faker = require('faker/');
 const args = require('yargs').argv;
 
 const { 
   getModelsWithRequestedProperties,
-  getSeedModelsWithRequestedProperties
+  getSeedModelsWithRequestedProperties,
  } = require('../../shared/models-utils.js');
 
+ const {
+  areAllpropertiesSeedsFilled,
+  typeOfSeedToGenerate,
+  getFakeModelsArray,
+  getRandomElementFromArray,
+ } = require('./utils/shared');
 
 /**
  * This constant holds all the custom models that the Loopback instance has available.
@@ -56,6 +61,8 @@ console.log("-------------INPUT-------------\n")
 console.log(`Model: '${singleModel}'`);
 console.log(`Number of records: ${numRecords}`);
 console.log("\n-------------------------------")
+
+
 
 /**
  * Get all the Seed models and processes them to only get the 
@@ -100,6 +107,8 @@ function getModelsSeedsFromSeedJSONModels( cb ) {
     .catch(err => cb(err));
 }
 
+
+
 /**
  * Get all the Seed models from the <code>arrayModels</code> array, creates
  * a the proper number of instances based on the wanted number of records 
@@ -143,46 +152,7 @@ async function seedModel( cb ) {
     
 }
 
-/**
- * Validates if the current seed model has all its properties_seeds filled.
- * @author Marcos Barrera del Río <elyomarcos@gmail.com>
- * @param {Object} Model - A given seed model object.
- * @param {callback} cb - The next callback to keep the flow on all the
- * <code>prepare process</code> on the file.
- */
-function areAllpropertiesSeedsFilled( Model ) {
-  const propertiesValues = Model.properties_seeds.map( property => Object.values(property)[0]);
-  return propertiesValues.every( property => property );
-}
 
-/**
- * Determines what kind of seed is going to need the current model.
- * There are two kind of seeds:
- * - The simple, which only runs a function that create fake data
- *   by using the seed JSON file for the given seed model.
- * - The complex, which also create fake models for the given seed
- *   model, however, this one also takes in consideration every
- *   related models by using the relations of the current model.
- * @param {string[]} relationsTypes - An array with all the relations from the
- * current model.
- * @returns {string} Returns the kind of seed that is going to be needed for the
- * given seed model.
- */
-function typeOfSeedToGenerate( relationsTypes ) {
-
-  let type = 'simpleSeed';
-
-  //Only 'hasMany' relations.
-  if( relationsTypes.length === 0 )
-    return 'simpleSeed'
-
-  else if( relationsTypes.includes('belongsTo') || relationsTypes.includes('hasMany') )
-    return 'complexSeed';
-
-  else throw "The relations that your model has are not supported currently.";
-  
-
-}
 
 /**
  * Creates a wanted number of records for the given model on the terminal.
@@ -194,7 +164,7 @@ function typeOfSeedToGenerate( relationsTypes ) {
  */
 function performSimpleSeed( Model, numberOfRecords, cb ) {
 
-  const fakeModelsArray = getfakeModelsArray( Model, numberOfRecords );
+  const fakeModelsArray = getFakeModelsArray( Model, numberOfRecords );
 
   models[singleModel].create(fakeModelsArray)
     .then(_ => cb(null))
@@ -202,31 +172,6 @@ function performSimpleSeed( Model, numberOfRecords, cb ) {
 
 }
 
-/**
- * Generates all the fake data that is going to be created on the database,
- * it creates a given number of records of the current seed model based on
- * the given number of records and it puts all these records on an array.
- * 
- * @param {*} Model - The current seed model from which the script is going
- * to generate fake data.
- * @param {number} numberOfRecords - The wanted number of records to be created.
- * @returns {Object[]} Returns an array with fake records of the current seed model.
- */
-function getfakeModelsArray( Model, numberOfRecords ) {
-
-  let fakeModel = { };
-  const fakeModelsArray = [];
-  for (let i = 0; i < numberOfRecords; i++) {
-    Model.properties_seeds.forEach( prop => 
-      fakeModel[Object.keys(prop)[0]] = faker.fake(Object.values(prop)[0])
-    )
-    fakeModelsArray.push(fakeModel);
-    fakeModel = { };
-  }
-
-  return fakeModelsArray;
-
-}
 
 
 async function performComplexSeed( Model, cb ) {
@@ -239,7 +184,7 @@ async function performComplexSeed( Model, cb ) {
 
     const mainLoopbackModel = models[Model.name];
 
-    const fakeModelsArray = getfakeModelsArray( Model, numRecords );
+    const fakeModelsArray = getFakeModelsArray( Model, numRecords );
 
 
     const finishedPromises = [];
@@ -289,6 +234,12 @@ async function performComplexSeed( Model, cb ) {
 
             if(relatedModelResults.length === 0) { //There aren't records.
               //TODO: Create new records then.
+              console.log(`The current ${Model.name} has a relation with the 
+                           ${relatedModel.name} model, however the ${relatedModel.name}
+                           model does not have records currently on the database,
+                           Do you want to create some records of it to get a more 
+                           accurate generated model?`);
+              //Put prompt functionality in here.
               return cb(`There are not ${relatedModel.name} existing records to make the relation insert`);
             } else {
 
@@ -318,16 +269,7 @@ async function performComplexSeed( Model, cb ) {
 
 }
 
-/**
- * Takes an array and returns a random element from it.
- *
- * @param {*} array - An array from which is going to be returned
- * a random element.
- * @returns {any} - A random element from the array param.
- */
-function getRandomElementFromArray( array ) {
-  return array[Math.floor(Math.random()*array.length)];
-}
+
 
 
 /**
---
+++ dev/seeds/utils/shared.js
@@ -0,0 +1,94 @@
+/** @module Seeds/Execute/Shared */
+
+/**
+ * Validates if the current seed model has all its properties_seeds filled.
+ * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+ * @param {Object} Model - A given seed model object.
+ * @param {callback} cb - The next callback to keep the flow on all the
+ * <code>prepare process</code> on the file.
+ */
+function areAllpropertiesSeedsFilled( Model ) {
+  const propertiesValues = Model.properties_seeds.map( property => Object.values(property)[0]);
+  return propertiesValues.every( property => property );
+}
+
+
+
+/**
+ * Determines what kind of seed is going to need the current model.
+ * There are two kind of seeds:
+ * - The simple, which only runs a function that create fake data
+ *   by using the seed JSON file for the given seed model.
+ * - The complex, which also create fake models for the given seed
+ *   model, however, this one also takes in consideration every
+ *   related models by using the relations of the current model.
+ * @param {string[]} relationsTypes - An array with all the relations from the
+ * current model.
+ * @returns {string} Returns the kind of seed that is going to be needed for the
+ * given seed model.
+ */
+function typeOfSeedToGenerate( relationsTypes ) {
+
+  let type = 'simpleSeed';
+
+  //Only 'hasMany' relations.
+  if( relationsTypes.length === 0 )
+    return 'simpleSeed'
+
+  else if( relationsTypes.includes('belongsTo') || relationsTypes.includes('hasMany') )
+    return 'complexSeed';
+
+  else throw "The relations that your model has are not supported currently.";
+  
+}
+
+
+
+/**
+ * Generates all the fake data that is going to be created on the database,
+ * it creates a given number of records of the current seed model based on
+ * the given number of records and it puts all these records on an array.
+ * 
+ * @param {*} Model - The current seed model from which the script is going
+ * to generate fake data.
+ * @param {number} numberOfRecords - The wanted number of records to be created.
+ * @returns {Object[]} Returns an array with fake records of the current seed model.
+ */
+function getFakeModelsArray( Model, numberOfRecords ) {
+
+  const faker = require('faker/');
+
+
+  let fakeModel = { };
+  const fakeModelsArray = [];
+  for (let i = 0; i < numberOfRecords; i++) {
+    Model.properties_seeds.forEach( prop => 
+      fakeModel[Object.keys(prop)[0]] = faker.fake(Object.values(prop)[0])
+    )
+    fakeModelsArray.push(fakeModel);
+    fakeModel = { };
+  }
+
+  return fakeModelsArray;
+
+}
+
+
+
+/**
+ * Takes an array and returns a random element from it.
+ *
+ * @param {*} array - An array from which is going to be returned
+ * a random element.
+ * @returns {any} - A random element from the array param.
+ */
+function getRandomElementFromArray( array ) {
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+module.exports = {
+  areAllpropertiesSeedsFilled,
+  typeOfSeedToGenerate,
+  getFakeModelsArray,
+  getRandomElementFromArray,
+}
