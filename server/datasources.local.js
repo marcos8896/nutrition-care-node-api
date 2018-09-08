49778c9a17e04880d4a7aecaf719eb3bd7e55e5f
marcos8896 <elyomarcos@gmail.com>
2018-09-08 14:12:37 -0500

    (#34) Change MySQL connection approach

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -7,9 +7,7 @@ module.exports = {
   },
   'mysql_ds': {
     // eslint-disable-next-line
-    'url': process.env.CLEARDB_DATABASE_URL ? 
-      process.env.CLEARDB_DATABASE_URL :
-      `mysql://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`,
+    'url': `mysql://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`,
     'name': 'mysql_ds',
     'connector': 'mysql',
   },
