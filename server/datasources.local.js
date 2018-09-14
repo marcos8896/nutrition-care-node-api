6a61b76eccf2785bc7194cf69010e84d59efe507
marcos8896 <elyomarcos@gmail.com>
2018-09-13 22:56:03 -0500

    (#31) - Add loopback-storage-component to the project

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,11 +6,25 @@ module.exports = {
     'connector': 'memory',
   },
   'mysql_ds': {
-    // eslint-disable-next-line
-    'url': process.env.JAWSDB_URL ? 
+    'url': process.env.JAWSDB_URL ?
            process.env.JAWSDB_URL :
+           // eslint-disable-next-line
            `mysql://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`,
     'name': 'mysql_ds',
     'connector': 'mysql',
+  },
+  'storage': {
+    'name': 'storage',
+    'connector': 'loopback-component-storage',
+    'provider': 'filesystem',
+    'root': './storage',
+    'allowedContentTypes': ['image/jpg', 'image/jpeg', 'image/png', 'image/tiff'],
+    'maxFileSize': 1 * 1024 * 1024,
+    'getFilename': function( fileInfo ) {
+
+      var { extension } = require( 'mime-types' );
+      return 'image-' + new Date().getTime() + '.' + extension( fileInfo.type );
+
+    },
   },
 };
