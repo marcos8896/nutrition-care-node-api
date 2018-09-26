2b284e012c1e7cae55be3e3264628fd743fe1796
marcos8896 <elyomarcos@gmail.com>
2018-09-26 18:58:47 -0500

    (#31) - Assert fullExerciseRegistration endpoint created correct data

--- common/services/__tests__/integration/exercise.js
+++ common/services/__tests__/integration/exercise.js
@@ -75,57 +75,69 @@ describe( 'ACLs from Exercise model', () => {
 
   });
 
-  it( 'should ALLOW an Administrator user to use fullExerciseRegistration endpoint', async () => {
+  it( 'should ALLOW an Administrator user to use fullExerciseRegistration endpoint',
 
-    const { credentials } = await createAuthenticatedAdmin( baseURL );
+    async () => {
 
-    const axios = require( 'axios' );
-    const formData = new FormData();
+      // Arrange
 
-    const multiPartAxios = axios.create({
-      baseURL: baseURL,
-      headers: {
-        ...formData.getHeaders(),
-        'Authorization': credentials.id,
-      },
+      const fileExists = require( 'file-exists' );
+      const resolve = require( 'path' ).resolve;
+
+      const { credentials } = await createAuthenticatedAdmin( baseURL );
+
+      const axios = require( 'axios' );
+      const formData = new FormData();
+
+      const multiPartAxios = axios.create({
+        baseURL: baseURL,
+        headers: {
+          ...formData.getHeaders(),
+          'Authorization': credentials.id,
+        },
+      });
+
+      const exercise = getFakeModelsArray( exerciseModel, 1 )[0];
+      const bodyAreas = getFakeModelsArray( bodyAreaModel, 5 );
+
+
+      // Act
+
+      // Create bodyAreas to be able to use them in following requests.
+      const createdBodyAreas = await BodyArea.create( bodyAreas );
+
+      const url = '/Exercises/fullExerciseRegistration';
+      formData.append( 'exercise', JSON.stringify({ name: exercise.name }) );
+      formData.append( 'bodyAreaDetails', JSON.stringify( createdBodyAreas ) );
+      formData.append( 'fileImage', fs.createReadStream(
+          resolve( './storage/exercises/exercise_example.jpg' )
+          ) );
+
+
+      const response = await multiPartAxios.post( url, formData )
+      .catch( e => {
+
+        throw e;
+
+      });
+
+      const registeredExercise = await Exercise.findById( response.data.exerciseId );
+
+
+      // Assert:
+      expect( response.status ).toBe( 200 );
+      expect( await Exercise.count() ).toBe( 1 );
+      expect( await BodyArea_Exercise_Detail.count() ).toBe( 5 );
+      expect( registeredExercise.name ).toBe( exercise.name );
+
+      const exerciseImageExist = await fileExists(
+        resolve( './storage/exercises/' + registeredExercise.imageName )
+      );
+
+      // The image exist on the exercises folder
+      expect( exerciseImageExist ).toBe( true );
+
+
     });
-
-    const exercise = getFakeModelsArray( exerciseModel, 1 )[0];
-    const bodyAreas = getFakeModelsArray( bodyAreaModel, 5 );
-
-    // Create bodyAreas to be able to use them in following requests.
-    const createdBodyAreas = await BodyArea.create( bodyAreas );
-
-
-    const resolve = require( 'path' ).resolve;
-
-    const url = '/Exercises/fullExerciseRegistration';
-    formData.append( 'exercise', JSON.stringify({ name: exercise.name }) );
-    formData.append( 'bodyAreaDetails', JSON.stringify( createdBodyAreas ) );
-    formData.append( 'fileImage', fs.createReadStream(
-        resolve( './storage/exercises/exercise_example.jpg' )
-        ) );
-
-
-    const response = await multiPartAxios.post( url, formData )
-    .catch( e => {
-
-      throw e;
-
-    });
-
-    // throw response.data;
-    // throw response.status;
-
-    // const response = await apiAdminAxios.post( '/BodyAreas', bodyArea )
-    //   .catch( e => e.response );
-    // const registeredBodyArea = response.data;
-
-    // const bodyAreaCount = await BodyArea.count();
-
-    // expect( registeredBodyArea.description ).toBe( bodyArea.description );
-    // expect( bodyAreaCount ).toBe( 1 );
-
-  });
 
 });
